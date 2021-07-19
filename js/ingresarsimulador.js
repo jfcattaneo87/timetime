0fa3da52768cbeec92c7a0e4cee6e451da78aeb5
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-07-19 16:34:55 -0500

    colocando notas

--- js/ingresarsimulador.js
+++ js/ingresarsimulador.js
@@ -3,8 +3,6 @@ const loginForm = document.getElementById("loginForm");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-
-
 loginButton.addEventListener("click", (e) => {
   e.preventDefault();
   const email = loginForm.email.value;
